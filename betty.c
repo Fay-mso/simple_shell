a4eb1540678de2bb8011530db8076bda6e447abf
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-06-28 04:40:59 -0700

    authors

--- betty.c
+++ betty.c
@@ -13,12 +13,12 @@ int main(void)
 	int i;
 	int sum = 0;
 
-	for (i = 1; i <= 10; i++)
+	for (i = 0; i <= 10; i++)
 	{
 	sum += i;
 	}
 
-	printf("The sum is: %d\n", sum);
+	printf("Sum: %d\n", sum);
 
 	return (0);
 }
