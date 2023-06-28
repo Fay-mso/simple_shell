f79bcf2c74744bb052f10c7b994cf8a6110068e5
Faith Kamakia <wfaithmuthoni578@gmail.com>
2023-06-28 00:09:39 -0700

    0

--- betty.c
+++ betty.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 
 /**
-* main - Entry point of the program
-* Description: Prints Hello World
-* Return: 0 (success)
-*
-*/
-
+ * main - Entry point of the program
+ *
+ * Description: Calculates the sum of numbers from 1 to 10
+ * and prints the result.
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	printf("%s", "Hello World!");
+	int i;
+	int sum = 0;
+
+	for (i = 1; i <= 10; i++)
+	{
+	sum += i;
+	}
+
+	printf("The sum is: %d\n", sum);
+
 	return (0);
 }
